5b90413e800f9ffaeba8b028769c2d3c05d19114
jwurtzle <jon.wurtzler@nerdery.com>
2019-12-07 12:02:44 -0600

    Updating test cases

--- src/Advent/ProgramAlarm.php
+++ src/Advent/ProgramAlarm.php
@@ -7,6 +7,8 @@ use DataLoader;
 class ProgramAlarm implements AdventOutputInterface
 {
     /**
+     * Guess 1: 106699 -- To Low
+     *
      * @var string
      */
     private $dataFile = '1202ProgramAlarm.txt';
@@ -17,15 +19,12 @@ class ProgramAlarm implements AdventOutputInterface
      */
     public function display()
     {
-        $program = $this->runProgram('1,1,1,4,99,5,6,0,99', true);
-        echo $program . "\n";
-        die;
-
         $input = DataLoader::loadFileAsArrayData($this->dataFile);
 
         $program = $this->runProgram($input[0], false);
+        $programString = implode(',', $program);
 
-        echo (sprintf('Part 1: Int at Index 0: %d of full Program: ', $program[0], implode(',', $program)) . "\n");
+        echo (sprintf('Part 1: Int at Index 0: %d of full Program: %s', $program[0], $programString) . "\n");
     }
 
     /**
@@ -37,9 +36,14 @@ class ProgramAlarm implements AdventOutputInterface
     {
         $program = explode(',', $input);
 
+        $this->outputCommandParts($program);
+        die;
+
         $index = 0;
-        while (isset($program[$index])) {
-            $this->runCommand($program, $index);
+        $terminateProgram = false;
+        while (!$terminateProgram) {
+            echo sprintf("Full Program at Step: %s \n", implode(',', $program));
+            $terminateProgram = $this->runCommand($program, $index);
 
             $index += 4;
         }
@@ -55,7 +59,7 @@ class ProgramAlarm implements AdventOutputInterface
      * @param int[] $program
      * @param int    $index
      *
-     * @return void
+     * @return bool
      */
     public function runCommand(&$program, $index = 0)
     {
@@ -64,15 +68,42 @@ class ProgramAlarm implements AdventOutputInterface
         $numTwo      = isset($program[$index + 2]) ? $program[$index + 2] : 0;
         $resultIndex = isset($program[$index + 3]) ? $program[$index + 3] : 0;
 
+        echo sprintf("Index: %s \n", $index);
+        echo sprintf("Op Code: %s \n", $opCode);
+        echo sprintf("Number 1: %s \n", $numOne);
+        echo sprintf("Number 1 - Value: %s \n", $program[$numOne]);
+        echo sprintf("Number 2: %s \n", $numTwo);
+        echo sprintf("Number 2 - Value: %s \n", $program[$numTwo]);
+        echo sprintf("Answer Index: %s \n", $resultIndex);
+
+        $newValue = 0;
+
         switch ($opCode) {
             case 1:
                 $program[$resultIndex] = $program[$numOne] + $program[$numTwo];
+                $newValue = $program[$numOne] + $program[$numTwo];
                 break;
             case 2:
                 $program[$resultIndex] = $program[$numOne] * $program[$numTwo];
+                $newValue = $program[$numOne] * $program[$numTwo];
                 break;
             case 99:
-                break;
+                return true;
+        }
+
+        echo sprintf("New Value: %s \n\n", $newValue);
+
+        return false;
+    }
+
+    public function outputCommandParts($program)
+    {
+        $len = count($program);
+        $index = 0;
+        while ($index < $len) {
+            $part = array_slice($program, $index, 4);
+            echo implode(',', $part) . "\n";
+            $index += 4;
         }
     }
 
