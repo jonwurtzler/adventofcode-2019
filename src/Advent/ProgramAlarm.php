8a2e5f4e2d79c187027d7f8adf5c63cca4dda722
jwurtzle <jon.wurtzler@nerdery.com>
2019-12-11 18:12:22 -0600

    Adding testing code and finding the answer to day 2 part 1

--- src/Advent/ProgramAlarm.php
+++ src/Advent/ProgramAlarm.php
@@ -6,8 +6,14 @@ use DataLoader;
 
 class ProgramAlarm implements AdventOutputInterface
 {
+    private $debug = true;
+
     /**
      * Guess 1: 106699 -- To Low
+     * Guess 2: 107699 -- To Low -random guess to get scope of diff
+     * Guess 3: 207699 -- To Low -random guess to get scope after walking through every step and things look right.
+     * I now realize I'm dumb and didn't read the instructions as fully as I should have. Missed replacing 1 and 2.
+     * Guess 4: 4714701 -- Finally, got it.
      *
      * @var string
      */
@@ -36,16 +42,19 @@ class ProgramAlarm implements AdventOutputInterface
     {
         $program = explode(',', $input);
 
-        $this->outputCommandParts($program);
-        die;
-
         $index = 0;
         $terminateProgram = false;
+        $stepCount = 1;
         while (!$terminateProgram) {
-            echo sprintf("Full Program at Step: %s \n", implode(',', $program));
+            if ($this->debug) {
+                echo sprintf("Full Program at Step %d:\n", $stepCount);
+                $this->outputCommandParts($program, 10);
+            }
+
             $terminateProgram = $this->runCommand($program, $index);
 
             $index += 4;
+            $stepCount++;
         }
 
         if ($returnSingle) {
@@ -68,43 +77,57 @@ class ProgramAlarm implements AdventOutputInterface
         $numTwo      = isset($program[$index + 2]) ? $program[$index + 2] : 0;
         $resultIndex = isset($program[$index + 3]) ? $program[$index + 3] : 0;
 
-        echo sprintf("Index: %s \n", $index);
-        echo sprintf("Op Code: %s \n", $opCode);
-        echo sprintf("Number 1: %s \n", $numOne);
-        echo sprintf("Number 1 - Value: %s \n", $program[$numOne]);
-        echo sprintf("Number 2: %s \n", $numTwo);
-        echo sprintf("Number 2 - Value: %s \n", $program[$numTwo]);
-        echo sprintf("Answer Index: %s \n", $resultIndex);
+        if ($this->debug) {
+            echo sprintf("Index: %s \n", $index);
+            echo sprintf("Op Code: %s \n", $opCode);
+            echo sprintf("Number 1: %s \n", $numOne);
+            echo sprintf("Number 1 - Value: %s \n", $program[$numOne]);
+            echo sprintf("Number 2: %s \n", $numTwo);
+            echo sprintf("Number 2 - Value: %s \n", $program[$numTwo]);
+            echo sprintf("Answer Index: %s \n", $resultIndex);
+        }
 
         $newValue = 0;
 
         switch ($opCode) {
             case 1:
-                $program[$resultIndex] = $program[$numOne] + $program[$numTwo];
                 $newValue = $program[$numOne] + $program[$numTwo];
+                $program[$resultIndex] = $newValue;
                 break;
             case 2:
-                $program[$resultIndex] = $program[$numOne] * $program[$numTwo];
                 $newValue = $program[$numOne] * $program[$numTwo];
+                $program[$resultIndex] = $newValue;
                 break;
             case 99:
                 return true;
         }
 
-        echo sprintf("New Value: %s \n\n", $newValue);
+        if ($this->debug) {
+            echo sprintf("New Value: %s \n\n", $newValue);
+        }
 
         return false;
     }
 
-    public function outputCommandParts($program)
+    /**
+     * Used for testing, just to give an output of the current array.
+     *
+     * @param array[] $program
+     * @param int     $length
+     *
+     * @return void
+     */
+    public function outputCommandParts($program, $length)
     {
         $len = count($program);
         $index = 0;
         while ($index < $len) {
-            $part = array_slice($program, $index, 4);
-            echo implode(',', $part) . "\n";
-            $index += 4;
+            $part = array_slice($program, $index, $length, true);
+            echo $index . " - " . implode(',', $part) . "\n";
+            $index += $length;
         }
+
+        echo "\n";
     }
 
 }
